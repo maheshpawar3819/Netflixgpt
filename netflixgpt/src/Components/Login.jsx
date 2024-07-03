d10a7ac38191b70d416acf91b7ca507405c474a8
Mahesh Pawar <maheshpawr3819@gmail.com>
2024-07-03 17:06:11 +0530

    add code but authorization issue found with firebase

--- netflixgpt/src/Components/Login.jsx
+++ netflixgpt/src/Components/Login.jsx
@@ -1,12 +1,110 @@
-import React from 'react'
-import Header from './Header'
-
+import React from "react";
+import Header from "./Header";
+import { logoheader } from "../Utils/logoimg";
+import { useState, useRef } from "react";
+import { checkvalidation } from "../Utils/validation";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../Utils/firebase";
 const Login = () => {
+  const [signinform, setSigninform] = useState(true);
+  const [errormsg, setErrormsg] = useState(null);
+
+  const name = useRef(null);
+  const email = useRef(null);
+  const password = useRef(null);
+  //   const name = useRef(null);
+
+  const checkfilds = () => {
+    //validate the form
+    const msg = checkvalidation(
+      email.current.value,
+      password.current.value
+      //   name.current.value
+    );
+    setErrormsg(msg);
+    console.log(msg);
+    if (msg) return; //if the user id and password was not valid dont go forword
+    //other wise go forword
+    //sign in sign up logic
+
+    if (!signinform) {
+      createUserWithEmailAndPassword(
+        auth,
+        email.current.value,
+        password.current.value
+      )
+        .then((userCredential) => {
+          // Signed up
+          const user = userCredential.user;
+          console.log(user);
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          setErrormsg(errorCode + "-" + errorMessage);
+        });
+    } else {
+    }
+  };
+
+  const istoggle = () => {
+    setSigninform(!signinform);
+  };
+
   return (
     <div>
-      <Header/>
+      <Header />
+      <div className="absolute">
+        <img src={logoheader} alt="background image" />
+      </div>
+      <form
+        className="bg-black relative p-12 w-3/12 mx-auto top-44 offset rounded-sm bg-opacity-85"
+        onSubmit={(e) => {
+          e.preventDefault();
+        }}
+      >
+        <p className="text-white text-4xl font-bold mb-4">
+          {signinform ? "Login" : "Sign Up"}
+        </p>
+        <br />
+        {!signinform ? (
+          <input
+            ref={name}
+            type="text"
+            placeholder="Full Name"
+            className="p-4 w-full  bg-gray-900 text-white rounded-sm"
+          />
+        ) : (
+          ""
+        )}
+        <input
+          ref={email}
+          type="text"
+          placeholder="Enter your e-mail"
+          className="p-4 my-5 w-full bg-gray-900 text-white rounded-sm"
+        />
+        <br />
+        <input
+          ref={password}
+          type="password"
+          placeholder="Password"
+          className="p-4 w-full mb-5 bg-gray-900 text-white rounded-sm"
+        />
+        <p className="text-red-500 font-bold py-2">{errormsg}</p>
+        <button
+          className="bg-red-600 hover:bg-red-800 w-full p-2 rounded-sm font-bold text-white tracking-wide"
+          onClick={checkfilds}
+        >
+          {signinform ? "Login" : "Sign UP"}
+        </button>
+        <p className="text-white my-3 cursor-pointer py-3" onClick={istoggle}>
+          {signinform
+            ? "New to Netflix? Sign up now"
+            : "Already have a account,login"}
+        </p>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
--- netflixgpt/src/Utils/validation.js
+++ netflixgpt/src/Utils/validation.js
@@ -1,15 +1,14 @@
-export const checkvalidation = (email, password, name) => {
-  const emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
-  const gmailRegex = /^[a-zA-Z0-9._%+-]+@gmail\.com$/;
-  const isEmail = emailRegex.test(email) || gmailRegex.test(email);
+export const checkvalidation = (email, password) => {
+  const isEmail = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email);
+
   let ispassword = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/.test(
     password
   );
-  let isname = /\b([A-ZÀ-ÿ][-,a-z. ']+[ ]*)+/.test(name);
+  // let isname = /\b([A-ZÀ-ÿ][-,a-z. ']+[ ]*)+/.test(name);
 
   if (!isEmail) return "Email is not valid";
   if (!ispassword) return "Password is not valid";
-  if (!isname) return "Plese enter valid name";
+  // if (!isname) return "Plese enter valid name";
 
   return null;
 };
