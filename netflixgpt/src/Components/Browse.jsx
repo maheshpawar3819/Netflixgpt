83ad7b841e9d865e9a9dd77495d81cc6f3ce89fc
Mahesh Pawar <maheshpawr3819@gmail.com>
2024-07-04 16:43:46 +0530

    successfully fetch api

--- netflixgpt/src/Components/Browse.jsx
+++ netflixgpt/src/Components/Browse.jsx
@@ -1,11 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Header from "./Header";
+import axios from "axios";
+import { apikey } from "../Utils/Constants";
 
 const Browse = () => {
+  const [data, setData] = useState([]);
+
+  const fetchletestmovies = () => {
+    axios
+      .get(
+        "https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1",
+        apikey
+      )
+      .then((response) => {
+        console.log(response?.data?.results);
+        setData(response?.data?.results[0]?.title);
+      })
+      .catch((error) => {
+        console.log("error was found", error);
+      });
+  };
+
+  useEffect(() => {
+    fetchletestmovies();
+  }, []);
+
   return (
     <div>
-      <Header />
-      <h1>i am borwse page</h1>
+      <div>
+        <Header />
+      </div>
     </div>
   );
 };
--- netflixgpt/src/Components/Header.jsx
+++ netflixgpt/src/Components/Header.jsx
@@ -2,11 +2,10 @@ import React from "react";
 import { netflixlogo } from "../Utils/logoimg";
 const Header = () => {
   return (
-    <div className="absolute px-8 py-5 bg-gradient-to-b from-black w-full z-30">
+    <div className="absolute px-8 py-5 top-0 bottom-0 bg-gradient-to-b from-black w-full z-30 h-16">
       <img src={netflixlogo} className="w-32" alt="" />
     </div>
   );
 };
 
 export default Header;
-  
