945cd9a962815e05a1986ae38e0b50a63e4de56c
Mahesh Pawar <maheshpawr3819@gmail.com>
2024-07-11 20:31:13 +0530

    update readme

--- netflixgpt/src/Components/Header.jsx
+++ netflixgpt/src/Components/Header.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { netflixlogo } from "../Utils/logoimg";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { toggleGptsearchview } from "../Utils/gptslice";
 import { Supportedlanguages } from "../Utils/Constants";
 import { changeLanguage } from "../Utils/configslice";
 const Header = () => {
   const dispatch = useDispatch();
+  const showGptsearch = useSelector((store) => store.gpt.showsearchsearch);
 
   const handleGptsearchclick = () => {
     dispatch(toggleGptsearchview());
@@ -21,23 +22,25 @@ const Header = () => {
       <img src={netflixlogo} className="w-32" alt="" />
 
       <div>
-        <select
-          className="bg-black hover:bg-gray-700 font-bold p-2 mr-10 rounded-md text-white cursor-pointer"
-          onClick={handleLanguagechange}
-        >
-          {Supportedlanguages.map((language) => {
-            return (
-              <option value={language.identifier} key={language.identifier}>
-                {language.name}
-              </option>
-            );
-          })}
-        </select>
+        {showGptsearch && (
+          <select
+            className="bg-black hover:bg-gray-700 font-bold p-2 mr-10 rounded-md text-white cursor-pointer"
+            onClick={handleLanguagechange}
+          >
+            {Supportedlanguages.map((language) => {
+              return (
+                <option value={language.identifier} key={language.identifier}>
+                  {language.name}
+                </option>
+              );
+            })}
+          </select>
+        )}
         <button
           className="bg-slate-600 p-2 rounded-md  text-white hover:text-red-400 hover:bg-slate-900"
           onClick={handleGptsearchclick}
         >
-          Gpt Search
+          {showGptsearch ? "Homepage" : "Gpt Search"}
         </button>
       </div>
     </div>
